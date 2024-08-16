f538faf71617ee9140c61b87098e25b210c9835b
KinnariyaMamaTanha <2823324228@qq.com>
2024-08-16 21:45:57 +0800

    2024.8.16

--- setup.sh
+++ setup.sh
@@ -2,3 +2,7 @@ conda create -n d2l python=3.9 -y
 conda activate d2l
 
 pip install torch==1.12.0 torchvision==0.13.0 d2l==0.17.6
+# for higher cuda version 
+# pip3 install torch torchvision torchaudio d2l==0.17.6
+# using conda
+# conda install pytorch torchvision torchaudio pytorch-cuda=11.8 d2l=0.17.6 -c pytorch -c nvidia
